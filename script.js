9453b77e3eabb9ad6da4c0755bbd952c61baec0e
Eren Sarıbay <eren99srb@gmail.com>
2022-08-28 23:01:16 +0300

    v1

---
+++ script.js
@@ -0,0 +1,14 @@
+var invis = document.getElementById("duyuru");
+function invisyap() {
+  invis.classList.toggle("invis");
+}
+
+var ucur = document.getElementById("imd");
+function firla() {
+  ucur.classList.toggle("ucurt");
+}
+
+var dondur = document.getElementById("img1");
+function doner() {
+  dondur.classList.toggle("dondurt");
+}
