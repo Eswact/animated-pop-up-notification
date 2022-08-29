6545caddcd19cc2a5880aea0ef8fe65dfeda0ff1
Eren Sarıbay <eren99srb@gmail.com>
2022-08-29 11:48:42 +0300

    json

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ var dondur = document.getElementById("img1");
 function doner() {
   dondur.classList.toggle("dondurt");
 }
+
+function getdata(){
+  const dm = document.getElementById("duymes");
+  fetch("veri.json")
+  .then(response=>response.json())
+  .then(veri=>{
+    for(cek of veri)
+    {
+      dm.innerHTML+= '<p id="dp"><span class="fw">DUYURU :</span> '+cek.duyuru+'</p>'     
+    }
+  })
+}
+getdata();
